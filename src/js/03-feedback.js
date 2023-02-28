d37c5c105adf5fbf0406f58bcd9941bfb6e51576
Olena <anyanyme@gmail.com>
2023-02-28 11:08:32 +0100

    update 03 feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
+
+
+const formData = {};
+
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('#email');
+const messageInput = document.querySelector('#message');
+
+form.addEventListener('input', throttle(onFormDataInput, 500));
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  evt.target.reset();
+  localStorage.removeItem('feedback-form-state');
+});
+
+initForm();
+    
+function onFormDataInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+
+}
+
+function initForm() { 
+    let prevFormFields = localStorage.getItem('feedback-form-state');
+    if (prevFormFields) {
+        prevFormFields = JSON.parse(prevFormFields);
+        // console.log(prevFormFields);
+        Object.entries(prevFormFields).forEach(([name, value]) => {
+            // console.log(name, value);
+            // console.log(form.elements);
+            formData[name] = value;
+            form.elements[name].value = value;          
+            
+        });
+       
+       
+    }
+};
+
+
 
